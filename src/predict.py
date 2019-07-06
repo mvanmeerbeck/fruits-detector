79cdcf2d75d91db3bfa0dce835254740a7c85c04
Maxime.Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2019-07-06 11:14:33 +0200

    saving model

--- src/predict.py
+++ src/predict.py
@@ -1,3 +1,3 @@
 from keras.models import load_model
 
-model = load_model('my_model.h5')
+model = load_model('model.h5')
--- src/train.py
+++ src/train.py
@@ -3,7 +3,6 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
-from keras.callbacks import ModelCheckpoint
 
 # dimensions of our images.
 img_width, img_height = 100, 100
@@ -12,7 +11,7 @@ train_data_dir = '../input/fruits-360/Training'
 validation_data_dir = '../input/fruits-360/Test'
 nb_train_samples = 56781
 nb_validation_samples = 19053
-epochs = 50
+epochs = 1
 batch_size = 32
 
 if K.image_data_format() == 'channels_first':
