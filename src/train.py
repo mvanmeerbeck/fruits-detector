12754b2f1b4412ccede50061fc9abf8d800f4f01
Maxime.Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2019-07-06 10:48:09 +0200

    save model, predict init

--- src/train.py
+++ src/train.py
@@ -69,14 +69,11 @@ validation_generator = test_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='categorical')
 
-checkpointer = ModelCheckpoint(filepath='cnn_from_scratch_fruits.hdf5', verbose=1, save_best_only=True)
-
 model.fit_generator(
     train_generator,
     steps_per_epoch=nb_train_samples // batch_size,
     epochs=epochs,
     validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size,
-    callbacks = [checkpointer])
+    validation_steps=nb_validation_samples // batch_size)
 
-model.save_weights('first_try.h5')
+model.save('model.h5')
