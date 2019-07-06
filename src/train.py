580ea0d19ac95ca13812554a8d0a164168f95e5e
Maxime.Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2019-07-06 10:12:43 +0200

    first model cnn

--- src/train.py
+++ src/train.py
@@ -1,111 +1,37 @@
-# First, we are going to load the file names and their respective target labels into numpy array!
-from sklearn.datasets import load_files
-import numpy as np
-
-train_dir = '../input/fruits-360/Training'
-test_dir = '../input/fruits-360/Test'
-
-
-def load_dataset(path):
-    data = load_files(path)
-    files = np.array(data['filenames'])
-    targets = np.array(data['target'])
-    target_labels = np.array(data['target_names'])
-    return files, targets, target_labels
-
-
-x_train, y_train, target_labels = load_dataset(train_dir)
-x_test, y_test, _ = load_dataset(test_dir)
-print('Loading complete!')
-
-print('Training set size : ', x_train.shape[0])
-print('Testing set size : ', x_test.shape[0])
-
-# Let's confirm the number of classes :p
-no_of_classes = len(np.unique(y_train))
-no_of_classes
-
-print(y_train[0:10])
-# target labels are numbers corresponding to class label. We need to change them to a vector of 81 elements.
-
-from keras.utils import np_utils
-y_train = np_utils.to_categorical(y_train,no_of_classes)
-y_test = np_utils.to_categorical(y_test,no_of_classes)
-y_train[0] # Note that only one element has value 1(corresponding to its label) and others are 0.
-
-# Now, we have to divide the validation set into test and validation set
-x_test,x_valid = x_test[7000:],x_test[:7000]
-y_test,y_vaild = y_test[7000:],y_test[:7000]
-print('Vaildation X : ',x_valid.shape)
-print('Vaildation y :',y_vaild.shape)
-print('Test X : ',x_test.shape)
-print('Test y : ',y_test.shape)
-
-x_train[0]
-# training data is just file names of images. We need to convert them into pixel matrix.
-
-# We just have the file names in the x set. Let's load the images and convert them into array.
-from keras.preprocessing.image import array_to_img, img_to_array, load_img
-
-@profile
-def convert_image_to_array(files):
-    images_as_array=[]
-    for file in files:
-        # Convert to Numpy Array
-        images_as_array.append(img_to_array(load_img(file)))
-    return images_as_array
-
-x_train = np.array(convert_image_to_array(x_train))
-print('Training set shape : ',x_train.shape)
-
-x_valid = np.array(convert_image_to_array(x_valid))
-print('Validation set shape : ',x_valid.shape)
-
-x_test = np.array(convert_image_to_array(x_test))
-print('Test set shape : ',x_test.shape)
-
-print('1st training image shape ',x_train[0].shape)
-
-print('1st training image as array',x_train[0]) # don't worry if you see only 255s..
-# there are elements will other values too :p
-
-# time to re-scale so that all the pixel values lie within 0 to 1
-x_train = x_train.astype('float32')/255
-x_valid = x_valid.astype('float32')/255
-x_test = x_test.astype('float32')/255
-x_train[0]
-
-#Let's visualize the first 10 training images!
-import matplotlib.pyplot as plt
-
-fig = plt.figure(figsize =(30,5))
-for i in range(10):
-    ax = fig.add_subplot(2,5,i+1,xticks=[],yticks=[])
-    ax.imshow(np.squeeze(x_train[i]))
-# Yummy fruits ;)
-
-#Simple CNN from scratch - we are using 3 Conv layers followed by maxpooling layers.
-# At the end we add dropout, flatten and some fully connected layers(Dense).
-
-from keras.models import Sequential
-from keras.layers import Conv2D,MaxPooling2D
-from keras.layers import Activation, Dense, Flatten, Dropout
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+
+# dimensions of our images.
+img_width, img_height = 100, 100
+
+train_data_dir = '../input/fruits-360/Training'
+validation_data_dir = '../input/fruits-360/Test'
+nb_train_samples = 56781
+nb_validation_samples = 19053
+epochs = 50
+batch_size = 32
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
 
 model = Sequential()
-model.add(Conv2D(filters = 16, kernel_size = 2,input_shape=(100,100,3),padding='same'))
+model.add(Conv2D(filters=16, kernel_size=2, input_shape=input_shape, padding='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=2))
 
-model.add(Conv2D(filters = 32,kernel_size = 2,activation= 'relu',padding='same'))
+model.add(Conv2D(filters=32, kernel_size=2, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=2))
 
-model.add(Conv2D(filters = 64,kernel_size = 2,activation= 'relu',padding='same'))
+model.add(Conv2D(filters=64, kernel_size=2, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=2))
 
-model.add(Conv2D(filters = 128,kernel_size = 2,activation= 'relu',padding='same'))
+model.add(Conv2D(filters=128, kernel_size=2, activation='relu', padding='same'))
 model.add(MaxPooling2D(pool_size=2))
 
 model.add(Dropout(0.3))
@@ -113,70 +39,44 @@ model.add(Flatten())
 model.add(Dense(150))
 model.add(Activation('relu'))
 model.add(Dropout(0.4))
-model.add(Dense(81,activation = 'softmax'))
+model.add(Dense(111, activation='softmax'))
 model.summary()
 
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
-print('Compiled!')
 
-batch_size = 32
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-checkpointer = ModelCheckpoint(filepath = 'cnn_from_scratch_fruits.hdf5', verbose = 1, save_best_only = True)
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-history = model.fit(x_train,y_train,
-        batch_size = 32,
-        epochs=30,
-        validation_data=(x_valid, y_vaild),
-        callbacks = [checkpointer],
-        verbose=2, shuffle=True)
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
 
-# load the weights that yielded the best validation accuracy
-model.load_weights('cnn_from_scratch_fruits.hdf5')
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
 
-# evaluate and print test accuracy
-score = model.evaluate(x_test, y_test, verbose=0)
-print('\n', 'Test accuracy:', score[1])
-#98% accuracy !!
+checkpointer = ModelCheckpoint(filepath='cnn_from_scratch_fruits.hdf5', verbose=1, save_best_only=True)
 
-# Let's visualize test prediction.
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size,
+    callbacks = [checkpointer])
 
-y_pred = model.predict(x_test)
-
-# plot a random sample of test images, their predicted labels, and ground truth
-fig = plt.figure(figsize=(16, 9))
-for i, idx in enumerate(np.random.choice(x_test.shape[0], size=16, replace=False)):
-    ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
-    ax.imshow(np.squeeze(x_test[idx]))
-    pred_idx = np.argmax(y_pred[idx])
-    true_idx = np.argmax(y_test[idx])
-    ax.set_title("{} ({})".format(target_labels[pred_idx], target_labels[true_idx]),
-                 color=("green" if pred_idx == true_idx else "red"))
-
-# Finally lets visualize the loss and accuracy wrt epochs
-
-import matplotlib.pyplot as plt
-
-plt.figure(1)
-
-# summarize history for accuracy
-
-plt.subplot(211)
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-
-# summarize history for loss
-
-plt.subplot(212)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+model.save_weights('first_try.h5')
